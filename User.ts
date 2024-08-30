b5d3df006433633161e1c07132402ae0db5241bd
Acquisition Portal <acquisitionportal@gmail.com>
2024-08-30 23:44:08 +0530

    first commit

---
+++ User.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
